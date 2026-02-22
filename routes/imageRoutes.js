b67235803b31e2c102d0eab89f79e9b18e012548
0shunya <shloksingh2805@gmail.com>
2026-02-22 20:59:40 +0530

    final

--- routes/imageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authMiddleware = require("../middleware/authMiddleware.js");
-const { generateImage } = require("../controllers/imageController.js");
-
-router.post("/generate", authMiddleware, generateImage);
-
-module.exports = router;
